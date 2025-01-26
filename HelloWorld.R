96b152a2cf39e7e6e5f0d9b003a8a515b033f6e2
Brandi <bbb@bbb.com>
2025-01-26 23:48:18 +0000

    first commit

---
+++ HelloWorld.R
@@ -0,0 +1,8 @@
+library(dplyr)
+library(TTR)
+library(quantmod)
+library(rio)
+symbol <- "AAPL"
+start_date <- "2023-01-01"
+end_date <- Sys.Date()
+
---
+++ impossible.r
@@ -0,0 +1,14 @@
+library (rio)
+library (dplyr)
+library(ggplot2)
+
+
+historic_data <- "https://raw.githubusercontent.com/H-B-P/d-and-d-sci-impossible-structures/main/data.csv"
+
+proposals_data <- "https://raw.githubusercontent.com/H-B-P/d-and-d-sci-impossible-structures/main/architect_proposals.txt"
+
+
+historic <-import(historic_data)
+
+proposals <-import(proposals_data,head=FALSE) %>% 
+ mutate("background of architect"=gsub("who was ","",V2)) 
